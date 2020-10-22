5708f4530eeab69f706d77378de48add244418e1
ReoOkumura <54521076+rokumura7@users.noreply.github.com>
2020-10-22 21:56:11 +0900

    fix HealthCheckController (#5)

--- app/controllers/HealthCheckController.scala
+++ app/controllers/HealthCheckController.scala
@@ -1,9 +1,9 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 @Singleton
-class HealthCheckController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HealthCheckController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
   def h(): Action[AnyContent] = Action(Ok)
 }
