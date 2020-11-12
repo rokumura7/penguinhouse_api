2f3ca53777ea68b25824ebd7c59076f047164f13
Ryoko Kawashima <ry.sep27@gmail.com>
2020-11-13 00:40:33 +0900

    add health check

--- app/controllers/HealthCheckController.scala
+++ app/controllers/HealthCheckController.scala
@@ -2,8 +2,23 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.api.db.Database
 
 @Singleton
-class HealthCheckController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HealthCheckController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
   def h(): Action[AnyContent] = Action(Ok)
+  def db(): Action[AnyContent] = Action {
+    var result = ""
+    val con = db.getConnection()
+    try {
+      val rs = con.createStatement.executeQuery("SELECT 200 AS res;")
+      while (rs.next()) {
+        result += rs.getString("res")
+      }
+    } finally {
+      con.close()
+    }
+    Ok(result)
+  }
 }
+
