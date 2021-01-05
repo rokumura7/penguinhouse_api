e04ff430b5ba531cdd02c1cfc33dbdb208593bef
rokumura7 <rokumura.8807@gmail.com>
2021-01-05 13:14:50 +0900

    fixed S3Client. using PenguinConf

--- app/aws/PenguinS3.scala
+++ app/aws/PenguinS3.scala
@@ -2,13 +2,12 @@ package aws
 
 import java.io.File
 import java.net.URI
-
-import com.typesafe.config.ConfigFactory
 import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCredentialsProvider}
 import software.amazon.awssdk.core.ResponseInputStream
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.model.{DeleteObjectRequest, DeleteObjectResponse, GetObjectRequest, GetObjectResponse, PutObjectRequest, PutObjectResponse}
 import software.amazon.awssdk.services.s3.{S3Client, S3Configuration}
+import utils.PenguinConf
 
 object PenguinS3 {
   def get(objKey: String): ResponseInputStream[GetObjectResponse] =
@@ -22,8 +21,8 @@ object PenguinS3 {
 
   private def client: S3Client = {
     val credential = AwsBasicCredentials.create(
-      ConfigFactory.load.getString("aws.s3.accessKey"),
-      ConfigFactory.load.getString("aws.s3.secretKey")
+      PenguinConf.AWS.S3.accessKey,
+      PenguinConf.AWS.S3.secretKey
     )
     val config = S3Configuration.builder()
       .pathStyleAccessEnabled(true)
@@ -31,7 +30,7 @@ object PenguinS3 {
     S3Client.builder()
       .region(Region.AP_NORTHEAST_1)
       .credentialsProvider(StaticCredentialsProvider.create(credential))
-      .endpointOverride(URI.create(ConfigFactory.load.getString("aws.s3.endpoint")))
+      .endpointOverride(URI.create(PenguinConf.AWS.S3.endpoint))
       .serviceConfiguration(config)
       .build()
   }
@@ -39,19 +38,19 @@ object PenguinS3 {
   private object RequestBuilder {
     def getReq(objKey: String): GetObjectRequest =
       GetObjectRequest.builder()
-        .bucket(ConfigFactory.load.getString("aws.s3.endpoint"))
+        .bucket(PenguinConf.AWS.S3.bucket)
         .key(objKey)
         .build()
 
     def putReq(objKey: String): PutObjectRequest =
       PutObjectRequest.builder()
-        .bucket(ConfigFactory.load.getString("aws.s3.endpoint"))
+        .bucket(PenguinConf.AWS.S3.bucket)
         .key(objKey)
         .build()
 
     def delReq(objKey: String): DeleteObjectRequest =
       DeleteObjectRequest.builder()
-        .bucket(ConfigFactory.load.getString("aws.s3.endpoint"))
+        .bucket(PenguinConf.AWS.S3.bucket)
         .key(objKey)
         .build()
   }
