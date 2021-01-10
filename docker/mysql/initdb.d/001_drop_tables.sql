389248d481da2d61fe7249e3e51ec637e515b255
rokumura7 <rokumura.8807@gmail.com>
2021-01-10 21:32:03 +0900

    fixed sql

---
+++ docker/mysql/initdb.d/001_drop_tables.sql
@@ -0,0 +1,9 @@
+USE penguinhouse;
+
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `contents`;
+DROP TABLE IF EXISTS `contents_categories`;
+DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `statuses`;
+DROP TABLE IF EXISTS `comments`;
+DROP TABLE IF EXISTS `images`;
