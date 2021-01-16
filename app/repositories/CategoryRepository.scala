204e5360f2c548eb8970a651c008e6f8723a4c41
rokumura7 <rokumura.8807@gmail.com>
2021-01-16 23:43:00 +0900

    impl rest actions on repository

--- app/repositories/CategoryRepository.scala
+++ app/repositories/CategoryRepository.scala
@@ -6,5 +6,9 @@ import repositories.impl.CategoryRepositoryImpl
 
 @ImplementedBy(classOf[CategoryRepositoryImpl])
 trait CategoryRepository extends Repository {
+  def findAll(): Seq[Category]
   def findBy(id: Int): Category
+  def save(category: Category): Int
+  def update(category: Category): Int
+  def delete(id: Int): Int
 }
