8d8f450feb5ecd8ca5c2921a6ca6673ab7667313
ReoOkumura <54521076+rokumura7@users.noreply.github.com>
2021-01-18 22:51:09 +0900

    impl rest actions on repository (#32)

--- app/repositories/AbstractRepository.scala
+++ app/repositories/AbstractRepository.scala
@@ -8,4 +8,7 @@ abstract class AbstractRepository(protected val db: Database) extends Repository
   def execute[A](sql: String)( func: ResultSet => A ): A = using(db.getConnection()) { con =>
     func(con.createStatement.executeQuery(sql))
   }
+  def executeUpdate(sql: String)( func: Int => Int ): Int = using(db.getConnection()) { con =>
+    func(con.createStatement.executeUpdate(sql))
+  }
 }
--- app/repositories/impl/CategoryRepositoryImpl.scala
+++ app/repositories/impl/CategoryRepositoryImpl.scala
@@ -8,8 +8,21 @@ import javax.inject.{Inject, Singleton}
 
 @Singleton
 class CategoryRepositoryImpl @Inject()(db: Database)  extends AbstractRepository(db) with CategoryRepository {
+  override def findAll(): List[Category] = {
+    execute(s"SELECT * FROM categories;") { rs =>
+      val list = List[Category]()
+      while (rs.next()) {
+        val builder = new CategoryBuilder()
+        builder
+          .name(rs.getString("name"))
+          .color(rs.getString("color"))
+        list :+ builder.build()
+      }
+      list
+    }
+  }
   override def findBy(id: Int): Category = {
-    val result = execute(s"SELECT * FROM categories WHERE category_id = ${id};") { rs =>
+    execute(s"SELECT * FROM categories WHERE category_id = ${id};") { rs =>
       val builder = new CategoryBuilder()
       while (rs.next()) {
         builder.categoryId(id)
@@ -18,6 +31,22 @@ class CategoryRepositoryImpl @Inject()(db: Database)  extends AbstractRepository
       }
       builder.build()
     }
-    result
+  }
+
+  override def save(category: Category): Int = {
+    executeUpdate(
+      s"INSERT INTO categories (name, color) " +
+        s"VALUES (${category.name}, ${category.color};") { rs => rs }
+  }
+
+  override def update(category: Category): Int = {
+    executeUpdate(
+      s"UPDATE categories " +
+        s"SET name = ${category.name}, color = ${category.color} " +
+        s"WHERE category_id = ${category.categoryId}") { rs => rs }
+  }
+
+  override def delete(id: Int): Int = {
+    executeUpdate(s"DELETE FROM categories WHERE category_id = ${id}") { rs => rs }
   }
 }
